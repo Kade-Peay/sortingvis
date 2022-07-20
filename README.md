bfba1b470d45a450b94958d8990a574c2cc2b72d
ThatOneDude14 <kadepeay@gmail.com>
2022-07-19 19:32:39 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+The idea with this is going to 3 pronged. First I want to get a few sorting 
+algorithms going. This shouldn't be too bad, easy peasy. 
+Next I'm going to add sound to this, so in real time I can hear which number 
+is being sorted. Of course I may have to delay the sorting so that all the sounds
+don't all play at once.
+Thirdly, and the most of it is going to be adding visualization, drawing what 
+the computer is doing.
---
+++ main.py
@@ -0,0 +1,117 @@
+import time 
+import random
+import pygame
+
+# Insertion sort
+def insertionSort(ar):
+    for i in range(1, len(ar)):
+        while ar[i-1] > ar[i] and i > 0:
+            ar[i-1], ar[i] = ar[i], ar[i-1]
+            i -= 1
+    return ar
+
+# Bubble sort 
+def bubbleSort(ar):
+    length = len(ar)
+    for i in range(length):
+        for j in range(length - i):
+            a = ar[j]
+            if a != ar[-1]:
+                b = ar[j + 1]
+                if a > b:
+                    ar[j] = b
+                    ar[j + 1] = a
+    return ar
+
+# Quick Sort 
+def quickSort(ar): 
+    if len(ar) < 2:
+        return ar 
+    else: 
+        pivot = ar[0]
+        less = [i for i in ar[1:] if i <= pivot]
+        greater = [i for i in ar[1:] if i > pivot]
+        return quickSort(less) + [pivot] + quickSort(greater)
+
+def createAr(len):
+    ar = []
+    for i in range(1, len+1):
+        ar.append(i)
+    return ar
+
+def main():
+
+    pygame.init()
+
+    # Set up the drawing window
+
+    screen = pygame.display.set_mode([1000,1000])
+
+    # Run until the user asks to quit
+    LENGTH = 50
+
+    ar = createAr(LENGTH)
+    random.shuffle(ar)
+
+    screen.fill((0, 0, 0))
+
+
+    # for ever number in the array, draw a rectangle of the same height
+    x_pos = 0
+    y_pos = 990
+    width = 15
+    height = 10
+
+    # make the rectangle variable
+    for num in ar:
+        x_pos += 16
+        height += num
+        y_pos -= num
+        rectangle = pygame.Rect(x_pos, y_pos, width, height)
+        pygame.draw.rect(screen, (255,255,255), rectangle)
+        
+        # This is what allows things to show up on screen 
+        pygame.display.flip()
+    running = True
+    while running:
+
+        # Did the user click the window close button?
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+        # Fill the background with white
+
+        # for num in ar:
+        #     x_pos += 10
+
+
+
+        # insertStart = time.time()
+        # insertionSort(ar)
+        # insertEnd = time.time()
+        # print(f'The time to do Insertion Sort was: {insertEnd - insertStart}\n')
+
+        # # scramble the recently sorted list
+        # random.shuffle(ar)
+
+        # bubbleStart = time.time()
+        # bubbleSort(ar)
+        # bubbleEnd = time.time()
+        # print(f'The time to do Bubble Sort was: {bubbleEnd - bubbleStart}\n')
+        
+        # # scramble the recently sorted list
+        # random.shuffle(ar)
+
+        # quickStart = time.time()
+        # quickSort(ar)
+        # quickEnd = time.time()
+        # print(f'The time to do Quick Sort was {quickEnd - quickStart}\n')
+
+
+    # time to quit
+    pygame.quit()
+
+
+if __name__ == '__main__':
+    main()
